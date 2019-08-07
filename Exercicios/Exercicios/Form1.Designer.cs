3fbe491918cdb09b3a894cbc6db2a7cea606a08d
Patrick <patrick.moura@cubotecnologia.com.br>
2019-08-07 11:20:40 -0300

    Exercicios_KLEBA

---
+++ Exercicios/Exercicios/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace Exercicios
+{
+    partial class Exercicios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textMult = new System.Windows.Forms.TextBox();
+            this.btnExe1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textExe2A = new System.Windows.Forms.TextBox();
+            this.textExe2B = new System.Windows.Forms.TextBox();
+            this.btnExe2 = new System.Windows.Forms.Button();
+            this.textResultExe2 = new System.Windows.Forms.TextBox();
+            this.textResultExe1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textSalAtual = new System.Windows.Forms.TextBox();
+            this.textSalReaj = new System.Windows.Forms.TextBox();
+            this.btnExe3 = new System.Windows.Forms.Button();
+            this.textResultExe3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textTmpViag = new System.Windows.Forms.TextBox();
+            this.textVelMedia = new System.Windows.Forms.TextBox();
+            this.btnExe4 = new System.Windows.Forms.Button();
+            this.textResultExe4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textNameFun = new System.Windows.Forms.TextBox();
+            this.textSalFixo = new System.Windows.Forms.TextBox();
+            this.textVolVendas = new System.Windows.Forms.TextBox();
+            this.btnExe5 = new System.Windows.Forms.Button();
+            this.textResultExe5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Exercicio1";
+            // 
+            // textMult
+            // 
+            this.textMult.Location = new System.Drawing.Point(85, 26);
+            this.textMult.Margin = new System.Windows.Forms.Padding(2);
+            this.textMult.Name = "textMult";
+            this.textMult.Size = new System.Drawing.Size(122, 20);
+            this.textMult.TabIndex = 1;
+            this.textMult.Text = "Digite um Numero";
+            // 
+            // btnExe1
+            // 
+            this.btnExe1.Location = new System.Drawing.Point(237, 24);
+            this.btnExe1.Name = "btnExe1";
+            this.btnExe1.Size = new System.Drawing.Size(145, 23);
+            this.btnExe1.TabIndex = 2;
+            this.btnExe1.Text = "Executa Exercicio 1";
+            this.btnExe1.UseVisualStyleBackColor = true;
+            this.btnExe1.Click += new System.EventHandler(this.BtnExe1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 65);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Exercicio2";
+            // 
+            // textExe2A
+            // 
+            this.textExe2A.Location = new System.Drawing.Point(86, 65);
+            this.textExe2A.Margin = new System.Windows.Forms.Padding(2);
+            this.textExe2A.Name = "textExe2A";
+            this.textExe2A.Size = new System.Drawing.Size(121, 20);
+            this.textExe2A.TabIndex = 4;
+            this.textExe2A.Text = "Digite um Numero A";
+            // 
+            // textExe2B
+            // 
+            this.textExe2B.Location = new System.Drawing.Point(228, 65);
+            this.textExe2B.Margin = new System.Windows.Forms.Padding(2);
+            this.textExe2B.Name = "textExe2B";
+            this.textExe2B.Size = new System.Drawing.Size(121, 20);
+            this.textExe2B.TabIndex = 5;
+            this.textExe2B.Text = "Digite um Numero B";
+            // 
+            // btnExe2
+            // 
+            this.btnExe2.Location = new System.Drawing.Point(367, 62);
+            this.btnExe2.Name = "btnExe2";
+            this.btnExe2.Size = new System.Drawing.Size(145, 23);
+            this.btnExe2.TabIndex = 14;
+            this.btnExe2.Text = "Executa Exercicio 2";
+            this.btnExe2.Click += new System.EventHandler(this.BtnExe2_Click);
+            // 
+            // textResultExe2
+            // 
+            this.textResultExe2.Location = new System.Drawing.Point(532, 67);
+            this.textResultExe2.Margin = new System.Windows.Forms.Padding(2);
+            this.textResultExe2.Name = "textResultExe2";
+            this.textResultExe2.Size = new System.Drawing.Size(121, 20);
+            this.textResultExe2.TabIndex = 7;
+            // 
+            // textResultExe1
+            // 
+            this.textResultExe1.Location = new System.Drawing.Point(405, 25);
+            this.textResultExe1.Margin = new System.Windows.Forms.Padding(2);
+            this.textResultExe1.Name = "textResultExe1";
+            this.textResultExe1.Size = new System.Drawing.Size(122, 20);
+            this.textResultExe1.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 104);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Exercicio3";
+            // 
+            // textSalAtual
+            // 
+            this.textSalAtual.Location = new System.Drawing.Point(85, 101);
+            this.textSalAtual.Margin = new System.Windows.Forms.Padding(2);
+            this.textSalAtual.Name = "textSalAtual";
+            this.textSalAtual.Size = new System.Drawing.Size(121, 20);
+            this.textSalAtual.TabIndex = 10;
+            this.textSalAtual.Text = "Digite Salario Atual";
+            // 
+            // textSalReaj
+            // 
+            this.textSalReaj.Location = new System.Drawing.Point(228, 104);
+            this.textSalReaj.Margin = new System.Windows.Forms.Padding(2);
+            this.textSalReaj.Name = "textSalReaj";
+            this.textSalReaj.Size = new System.Drawing.Size(121, 20);
+            this.textSalReaj.TabIndex = 11;
+            this.textSalReaj.Text = "Digite Valor de Reajuste";
+            // 
+            // btnExe3
+            // 
+            this.btnExe3.Location = new System.Drawing.Point(367, 101);
+            this.btnExe3.Name = "btnExe3";
+            this.btnExe3.Size = new System.Drawing.Size(145, 23);
+            this.btnExe3.TabIndex = 12;
+            this.btnExe3.Text = "Executa Exercicio 3";
+            this.btnExe3.UseVisualStyleBackColor = true;
+            this.btnExe3.Click += new System.EventHandler(this.BtnExe3_Click);
+            // 
+            // textResultExe3
+            // 
+            this.textResultExe3.Location = new System.Drawing.Point(532, 101);
+            this.textResultExe3.Margin = new System.Windows.Forms.Padding(2);
+            this.textResultExe3.Name = "textResultExe3";
+            this.textResultExe3.Size = new System.Drawing.Size(121, 20);
+            this.textResultExe3.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 146);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Exercicio4";
+            // 
+            // textTmpViag
+            // 
+            this.textTmpViag.Location = new System.Drawing.Point(85, 139);
+            this.textTmpViag.Margin = new System.Windows.Forms.Padding(2);
+            this.textTmpViag.Name = "textTmpViag";
+            this.textTmpViag.Size = new System.Drawing.Size(121, 20);
+            this.textTmpViag.TabIndex = 16;
+            this.textTmpViag.Text = "Digite tempo Gasto";
+            // 
+            // textVelMedia
+            // 
+            this.textVelMedia.Location = new System.Drawing.Point(228, 139);
+            this.textVelMedia.Margin = new System.Windows.Forms.Padding(2);
+            this.textVelMedia.Name = "textVelMedia";
+            this.textVelMedia.Size = new System.Drawing.Size(121, 20);
+            this.textVelMedia.TabIndex = 17;
+            this.textVelMedia.Text = "Digite Velocidade Media";
+            // 
+            // btnExe4
+            // 
+            this.btnExe4.Location = new System.Drawing.Point(367, 136);
+            this.btnExe4.Name = "btnExe4";
+            this.btnExe4.Size = new System.Drawing.Size(145, 23);
+            this.btnExe4.TabIndex = 18;
+            this.btnExe4.Text = "Executa Exercicio 4";
+            this.btnExe4.UseVisualStyleBackColor = true;
+            this.btnExe4.Click += new System.EventHandler(this.BtnExe4_Click);
+            // 
+            // textResultExe4
+            // 
+            this.textResultExe4.Location = new System.Drawing.Point(532, 138);
+            this.textResultExe4.Margin = new System.Windows.Forms.Padding(2);
+            this.textResultExe4.Name = "textResultExe4";
+            this.textResultExe4.Size = new System.Drawing.Size(339, 20);
+            this.textResultExe4.TabIndex = 19;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 179);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Exercicio5";
+            // 
+            // textNameFun
+            // 
+            this.textNameFun.Location = new System.Drawing.Point(86, 179);
+            this.textNameFun.Margin = new System.Windows.Forms.Padding(2);
+            this.textNameFun.Name = "textNameFun";
+            this.textNameFun.Size = new System.Drawing.Size(121, 20);
+            this.textNameFun.TabIndex = 21;
+            this.textNameFun.Text = "Digite Nome Funcionario";
+            // 
+            // textSalFixo
+            // 
+            this.textSalFixo.Location = new System.Drawing.Point(228, 179);
+            this.textSalFixo.Margin = new System.Windows.Forms.Padding(2);
+            this.textSalFixo.Name = "textSalFixo";
+            this.textSalFixo.Size = new System.Drawing.Size(121, 20);
+            this.textSalFixo.TabIndex = 22;
+            this.textSalFixo.Text = "Digite Salario Fixo";
+            // 
+            // textVolVendas
+            // 
+            this.textVolVendas.Location = new System.Drawing.Point(367, 179);
+            this.textVolVendas.Margin = new System.Windows.Forms.Padding(2);
+            this.textVolVendas.Name = "textVolVendas";
+            this.textVolVendas.Size = new System.Drawing.Size(121, 20);
+            this.textVolVendas.TabIndex = 23;
+            this.textVolVendas.Text = "Digite Vol Vendas";
+            // 
+            // btnExe5
+            // 
+            this.btnExe5.Location = new System.Drawing.Point(493, 177);
+            this.btnExe5.Name = "btnExe5";
+            this.btnExe5.Size = new System.Drawing.Size(145, 23);
+            this.btnExe5.TabIndex = 24;
+            this.btnExe5.Text = "Executa Exercicio 5";
+            this.btnExe5.UseVisualStyleBackColor = true;
+            this.btnExe5.Click += new System.EventHandler(this.BtnExe5_Click);
+            // 
+            // textResultExe5
+            // 
+            this.textResultExe5.Location = new System.Drawing.Point(658, 176);
+            this.textResultExe5.Margin = new System.Windows.Forms.Padding(2);
+            this.textResultExe5.Name = "textResultExe5";
+            this.textResultExe5.Size = new System.Drawing.Size(339, 20);
+            this.textResultExe5.TabIndex = 25;
+            // 
+            // Exercicios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1223, 366);
+            this.Controls.Add(this.textResultExe5);
+            this.Controls.Add(this.btnExe5);
+            this.Controls.Add(this.textVolVendas);
+            this.Controls.Add(this.textSalFixo);
+            this.Controls.Add(this.textNameFun);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textResultExe4);
+            this.Controls.Add(this.btnExe4);
+            this.Controls.Add(this.textVelMedia);
+            this.Controls.Add(this.textTmpViag);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textResultExe3);
+            this.Controls.Add(this.btnExe3);
+            this.Controls.Add(this.textSalReaj);
+            this.Controls.Add(this.textSalAtual);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textResultExe1);
+            this.Controls.Add(this.textResultExe2);
+            this.Controls.Add(this.btnExe2);
+            this.Controls.Add(this.textExe2B);
+            this.Controls.Add(this.textExe2A);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnExe1);
+            this.Controls.Add(this.textMult);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Exercicios";
+            this.Text = "Exercicios";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textMult;
+        private System.Windows.Forms.Button btnExe1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textExe2A;
+        private System.Windows.Forms.TextBox textExe2B;
+        private System.Windows.Forms.Button btnExe2;
+        private System.Windows.Forms.TextBox textResultExe2;
+        private System.Windows.Forms.TextBox textResultExe1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textSalAtual;
+        private System.Windows.Forms.TextBox textSalReaj;
+        private System.Windows.Forms.Button btnExe3;
+        private System.Windows.Forms.TextBox textResultExe3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textTmpViag;
+        private System.Windows.Forms.TextBox textVelMedia;
+        private System.Windows.Forms.Button btnExe4;
+        private System.Windows.Forms.TextBox textResultExe4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textNameFun;
+        private System.Windows.Forms.TextBox textSalFixo;
+        private System.Windows.Forms.TextBox textVolVendas;
+        private System.Windows.Forms.Button btnExe5;
+        private System.Windows.Forms.TextBox textResultExe5;
+    }
+}
+
---
+++ Exercicios/Exercicios/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercicios
+{
+    public partial class Exercicios : Form
+    {
+        public Exercicios()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnExe1_Click(object sender, EventArgs e)
+        {
+            var numero = Convert.ToInt32(textMult.Text);
+            textResultExe1.Text = (numero * numero).ToString();
+             
+
+
+        }
+        private void BtnExe2_Click(object sender, EventArgs e)
+        {
+            var numA = Convert.ToDouble(textExe2A.Text);
+            var numB = Convert.ToDouble(textExe2B.Text);
+
+            textResultExe2.Text = Math.Pow(((numA * 9) / (87 - numB)), 2).ToString();
+
+        }
+
+        private void BtnExe3_Click(object sender, EventArgs e)
+        {
+            var salAtual = Convert.ToDouble(textSalAtual.Text);
+            var reajuste = (Convert.ToDouble(textSalReaj.Text))/100;
+            textResultExe3.Text = (salAtual + (salAtual * reajuste)).ToString("C");
+            
+        }
+
+        private void BtnExe4_Click(object sender, EventArgs e)
+        {
+            var velMedia = Convert.ToDouble(textVelMedia.Text);
+            var tempPerc = TimeSpan.Parse(textTmpViag.Text).TotalHours;
+            var distancia = velMedia * tempPerc;
+            var litros = distancia / 12;
+
+            textResultExe4.Text = $"A Distancia percorrida foi: {distancia}km e gastou {litros}litros";
+
+        }
+
+        private void BtnExe5_Click(object sender, EventArgs e)
+        {
+            var salFixo = Convert.ToDouble(textSalFixo.Text);
+            var volVendas = Convert.ToDouble(textVolVendas.Text);
+            var salarioAtuali = (salFixo + (volVendas * 0.10)).ToString("C");
+            textResultExe5.Text = $"O Salario do {textNameFun.Text} este mês é {salarioAtuali}";
+        }
+    }
+}
